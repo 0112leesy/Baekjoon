839e93393d70f5488b5e79f25bb3890727dc6296
seyoung <0112leesy@naver.com>
2022-01-04 22:03:05 +0900

    동적계획법1:1932번

--- 동적계획법1/1932번/Main.java
+++ 동적계획법1/1932번/Main.java
@@ -35,10 +35,10 @@ public class Main {
 		System.out.println(func(0,0));
 
 	}
-	// 맨 마지막 행부터 탐색을 시작하여, 결국 맨 첫번째 행에 최적 값이 오도록 한다
+	// 맨 마지막 행부터 탐색이 시작되며, 결국 맨 첫번째 행에 최적 값이 오도록 한다
 	// 해당 행의 최적 값은 다음 행의 인접한 최대 값과 현재의 값을 더한 것이다
 	static int func(int x, int idx) {
-		if(x == N-1) return sum[x][idx];
+		if(x == N-1) return sum[x][idx]; // 맨 마지막 행에 다다랐을 때, 바로 값을 리턴한다
 		
 		if(sum[x][idx] == -1) {
 			sum[x][idx] = arr[x][idx] + Math.max(func(x+1, idx), func(x+1, idx+1));
