8f7afb36efd5bef03985b9152dfef75ed6a741de
seyoung <0112leesy@naver.com>
2022-02-20 00:01:43 +0900

    이분 탐색:1654번

--- 이분 탐색/1654번/Main.java
+++ 이분 탐색/1654번/Main.java
@@ -1,50 +1,47 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.StringTokenizer;
  
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-		int K = Integer.parseInt(st.nextToken()); 
-		int N = Integer.parseInt(st.nextToken()); 
- 
-		int[] arr = new int[K];
-		
-		long max = 0;
-		
-		for (int i = 0; i < K; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			if(max < arr[i]) {
-				max = arr[i];
-			}
-		}
 	
-		max++; 
- 
-		long min = 0; 
-		long mid = 0; 
- 
-		while (min < max) { 
-			mid = (max + min) / 2;
-			long count = 0; 
-			for (int i = 0; i < arr.length; i++) {
-				count += (arr[i] / mid);
-			}
-			
-			if(count < N) {
-				max = mid;
-			}
-			else {
-				min = mid + 1;
-			}
-			
- 
+	static int[] arr;
+	static int N, K;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		K = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		
+		arr = new int[K];
+		for(int i=0; i<K; i++) arr[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(arr);
+		
+        // long으로 선언하는 것에 주의
+		long l = 1;
+		long r = arr[K-1];
+		while(l < r) {
+			long mid = (l + r + 1) / 2;
+			if(check(mid)) l = mid;
+			else r = mid - 1;
 		}
-		System.out.println(min - 1);
+		
+		System.out.println(l);
+		
 	}
+	
+	static boolean check(long x) {
+		long cnt = 0;
+		
+		for(int i=0; i<K; i++) {
+			cnt += (arr[i] / x);
+		}
+		
+		if(cnt >= N) return true;
+		else return false;
+	}
+	
 }
