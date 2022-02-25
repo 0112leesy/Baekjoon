fce8cab465570020ab31a1b3eaedc8023b530ffa
seyoung <0112leesy@naver.com>
2022-02-25 23:41:54 +0900

    DFS와 BFS:2606번

--- DFS와 BFS/2606번/Main.java
+++ DFS와 BFS/2606번/Main.java
@@ -1,42 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int map[][];
-	static boolean visit[];
-	static int n, m, v;
-	static int count = 0;
 	
-	public static int dfs(int i) {
-		visit[i] = true;
-		
-		for(int j=1; j<=n; j++) {
-			if(map[i][j] == 1 && visit[j] == false) {
-				count ++;
-				dfs(j);
-			}
-		}
-		return count;
-	}
-	
-	public static void main(String[] args) throws IOException{
+	static ArrayList<ArrayList<Integer>> GraphList;
+	static boolean visited[];
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine()); // 컴퓨터 수(정점)
-		m = Integer.parseInt(br.readLine()); // 연결된 컴퓨터 쌍의 수(간선)
-		v = 1;	// 탐색 시장할 정점의 번호
-		map = new int[n+1][n+1];	// 각 정점간 탐색 경로를 저장할 배열
-		visit = new boolean[n+1];	// 정점의 탐색 여부 체크
+		Integer N = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		
-		StringTokenizer st;
-		for(int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
+		visited = new boolean[N+1];
+		GraphList = new ArrayList<>();
+		for(int i=0; i<=N; i++) {
+			GraphList.add(new ArrayList<Integer>());
+		}
+		for(int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			map[a][b] = map[b][a]= 1;
+			GraphList.get(a).add(b);
+			GraphList.get(b).add(a);
 		}
 		
-		System.out.println(dfs(1));
+		dfs(1);
+		
+		int ans = 0;
+		for(int i=1; i<=N; i++) {
+			if(visited[i]) ans++;
+		}
+		System.out.println(ans - 1);
+		
 	}
+	
+	static void dfs(int x) {
+		visited[x] = true;
+		for(int y : GraphList.get(x)) {
+			if(!visited[y]) {
+				dfs(y);
+			}
+		}
+		
+	}
+
 }
