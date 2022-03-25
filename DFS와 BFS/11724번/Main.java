4cbf6ee2d6399dc8e607e9d0005d764b0c4d979d
seyoung <0112leesy@naver.com>
2022-03-25 13:53:54 +0900

    DFS와 BFS:11724번

---
+++ DFS와 BFS/11724번/Main.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static boolean[] visited;
+	static ArrayList<ArrayList<Integer>> graph;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		graph = new ArrayList<>();
+		for(int i=0; i<=N; i++) graph.add(new ArrayList<Integer>());
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int p = Integer.parseInt(st.nextToken());
+			int q = Integer.parseInt(st.nextToken());
+			
+			graph.get(p).add(q);
+			graph.get(q).add(p);
+		}
+		
+		visited = new boolean[N+1];
+		
+		int cnt = 0;
+		for(int i=1; i<=N; i++) {
+			if(!visited[i]) {
+				dfs(i);
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+		
+	}
+	
+	static void dfs(int x) {
+		visited[x] = true;
+		
+		for(int n : graph.get(x)) {
+			if(!visited[n]) dfs(n);
+		}
+	}
+	
+}
