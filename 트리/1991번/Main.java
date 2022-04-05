0c3eb5129fe09875b68912ff12c91b76c0bb05c5
seyoung <0112leesy@naver.com>
2022-04-05 14:16:48 +0900

    트리:1991번

--- 트리/1991번/Main.java
+++ 트리/1991번/Main.java
@@ -1,100 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-class Node {
-	char data;
-	Node left;
-	Node right;
-	
-	Node(char data) {
-		this.data = data;
-	}
-}
-
-class Tree {
-	Node root; //루트 노드 처음엔 null 상태 
-	
-	public void createNode(char data, char leftData, char rightData) {
-		if(root == null) { //아무것도 없는 초기 상태 - A 루트 노드 생성 
-			root = new Node(data);
-			
-			//좌우 값이 있는 경우에만 노드 생성 
-			if(leftData != '.') { 
-				root.left = new Node(leftData);
-			}
-			if(rightData != '.') {
-				root.right = new Node(rightData);
-			}
-		} else { //초기상태가 아니면 어디에 들어가야할지 찾아야함 - A 루트 노드 이후 
-			searchNode(root, data, leftData, rightData);
-		}
-	}
-	
-    //여기에서 root는 매개 변수로 들어온 로컬변수 root임을 주의 
-	public void searchNode(Node root, char data, char leftData, char rightData) { 
-		if(root == null) { //도착한 노드가 null이면 재귀 종료 - 찾을(삽입할) 노드 X
-			return;
-		} else if(root.data == data) { //들어갈 위치를 찾았다면 
-			if(leftData != '.') { //.이 아니라 값이 있는 경우에만 좌우 노드 생성 
-				root.left = new Node(leftData);
-			}
-			if(rightData != '.') {
-				root.right = new Node(rightData);
-			}
-		} else { //아직 찾지못했고 탐색할 노드가 남아 있다면 
-			searchNode(root.left, data, leftData, rightData); //왼쪽 재귀 탐색 
-			searchNode(root.right, data, leftData, rightData); //오른쪽 재귀 탐색 
-		}
-	}
-	
-	// 전위순회 : 루트 -> 좌 -> 우 
-	public void preorder(Node root){
-		System.out.print(root.data);
-		if(root.left!=null) preorder(root.left);
-        if(root.right!=null) preorder(root.right);
-	}
-	
-	// 중위순회 : 좌 -> 루트 -> 우
-	public void inorder(Node root){
-		if(root.left!=null) inorder(root.left);
-		System.out.print(root.data);
-		if(root.right!=null) inorder(root.right);
-	}
-	
-	// 후위순회 : 좌 -> 우 -> 루트 
-	public void postorder(Node root){
-		if(root.left!=null) postorder(root.left);
-		if(root.right!=null) postorder(root.right);
-		System.out.print(root.data);
-	}
-	
-}
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
 		Tree tree = new Tree();
 		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			char root = st.nextToken().charAt(0);
-			char left = st.nextToken().charAt(0);
-			char right = st.nextToken().charAt(0);
-			
-			tree.createNode(root, left, right);
-			
+			String str = br.readLine();
+			tree.insert(str.charAt(0), str.charAt(2), str.charAt(4));
 		}
-
-        tree.preorder(tree.root);
-        System.out.println();
-
-        tree.inorder(tree.root);
-        System.out.println();
-
-        tree.postorder(tree.root);
+		
+		tree.PreOrder(tree.root);
+		System.out.println();
+		tree.InOrder(tree.root);
+		System.out.println();
+		tree.PostOrder(tree.root);
 	}
+	
+	
+}
 
+class Node {
+	char data;
+	Node left, right;
+	
+	Node(char c){
+		data = c;
+		left = null;
+		right = null;
+	}
+}
+
+class Tree{
+	Node root;
+	
+	Tree(){
+		root = null;
+	}
+	
+	// 주어진 입력을 바탕으로 노드를 추가함
+	void insert(char data, char left_data, char right_data) {
+		if(root == null) {// root가 null인 초기상태에서는 data를 갖는 새로운 노드 생성 
+			root = new Node(data);
+			if(left_data != '.') root.left = new Node(left_data);
+			if(right_data != '.') root.right = new Node(right_data);
+		}
+		// 초기상태가 아닐 경우에는 data를 갖는 해당 노드를 찾아야 함
+		else search(root, data, left_data, right_data);
+	}
+	
+	void search(Node x, char data, char left_data, char right_data) {
+		if(x == null) return;
+		
+		else if(x.data == data) { // data를 갖는 노드를 찾은 경우, 왼쪽 오른쪽 자식 노드 생성 후 지정
+			if(left_data != '.') x.left = new Node(left_data);
+			if(right_data !='.') x.right = new Node(right_data);
+		}
+		
+		else { // data를 갖는 노드가 아닌 경우, 왼쪽 오른쪽 자식으로 이동해 다시 탐색
+			search(x.left, data, left_data, right_data);
+			search(x.right, data, left_data, right_data);
+		}
+	}
+	
+	void PreOrder(Node root) {
+		System.out.print(root.data);
+		if(root.left != null) PreOrder(root.left);
+		if(root.right != null) PreOrder(root.right);
+	}
+	
+	void InOrder(Node root) {
+		if(root.left != null) InOrder(root.left);
+		System.out.print(root.data);
+		if(root.right != null) InOrder(root.right);
+	}
+	
+	void PostOrder(Node root) {
+		if(root.left != null) PostOrder(root.left);
+		if(root.right != null) PostOrder(root.right);
+		System.out.print(root.data);
+	}
 }
