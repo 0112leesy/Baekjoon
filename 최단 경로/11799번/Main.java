74e2b8cdb34d90a467fa5834ab39e41908dcbc86
seyoung <0112leesy@naver.com>
2022-09-20 13:13:45 +0900

    최단 경로:11799번

--- 최단 경로/11799번/Main.java
+++ 최단 경로/11799번/Main.java
@@ -3,26 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main { // 시간초과
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int m = Integer.parseInt(br.readLine());
 		
-		ArrayList<ArrayList<Integer[]>> graph = new ArrayList<>();
-		for(int i=0; i<=n; i++) graph.add(new ArrayList<>());
+		ArrayList<Node>[] graph = new ArrayList[n+1];
+		for(int i=1; i<=n; i++) graph[i] = new ArrayList<>();
 		
 		for(int i=0; i<m; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
-			graph.get(from).add(new Integer[] {to, cost});
+			graph[from].add(new Node(to, cost));
 		}
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -30,55 +30,64 @@ public class Main { // 시간초과
 		int E = Integer.parseInt(st.nextToken());
 		
 		int[] dist = new int[n+1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
+		Arrays.fill(dist, 100000000); // MaxValue로 했을 때 메모리초과
 		
-		PriorityQueue<Integer[]> pq = new PriorityQueue<>(new Comparator<Integer[]>() {
-			// 거리 기준으로 오름차순
-			@Override
-			public int compare(Integer[] o1, Integer[] o2) {
-				// TODO Auto-generated method stub
-				return o1[1] - o2[1];
-			}
-			
-		});
+		PriorityQueue<Node> pq = new PriorityQueue<>();
 		
 		dist[S] = 0;
-		pq.offer(new Integer[] {S, dist[S]});
+		pq.offer(new Node(S, dist[S]));
 		
 		int[] route = new int[n+1];
 
-		//String[] route = new String[n+1];
-		//route[S] = Integer.toString(S);
-		
-		//ArrayList<ArrayList<Integer>> route = new ArrayList<>();
-		//for(int i=0; i<=n; i++) route.add(new ArrayList<>());
-		//route.get(S).add(S);
-		
-		// (V+E)logV
 		while(!pq.isEmpty()) {
-			Integer[] tmp = pq.poll();
-			int now = tmp[0];
-			if(now == E) {
+			Node now = pq.poll();
+
+			if(now.to == E) {
 				break;
 			}
-			int now_dist = tmp[1];
-			for(Integer[] e : graph.get(now)) {// [to, cost]
-				if(dist[e[0]] >= dist[now] + e[1]) {
-					//route[e[0]] = route[now] + " " +e[0];
-					
-					//route.get(e[0]).clear();
-					//for(int city : route.get(now)) route.get(e[0]).add(city);
-					//route.get(e[0]).add(e[0]);
-					
-					dist[e[0]] = dist[now] + e[1];
-					pq.offer(new Integer[] {e[0], dist[e[0]]});
+			
+			if(dist[now.to] < now.cost) continue; // 다음도시의 dist가 다음도시로 가는 cost보다 적으면 continue
+
+			for(Node next : graph[now.to]) {// [to, cost]
+				if(dist[next.to] > dist[now.to] + next.cost) {
+					dist[next.to] = dist[now.to] + next.cost;
+					pq.offer(new Node(next.to, dist[next.to]));
+					route[next.to] = now.to;
 				}
 			}
 		}
 		System.out.println(dist[E]);
-		/*System.out.println(route.get(E).size());
+
+		int cnt = 1;
+		Stack<Integer> stack = new Stack<>();
+		stack.push(E);
+		while(route[E] != 0) {
+			cnt++;
+			stack.push(route[E]);
+			E = route[E];
+		}
+		System.out.println(cnt);
+		
 		StringBuilder sb = new StringBuilder();
-		for(int city : route.get(E)) sb.append(city+" ");
-		System.out.println(sb);*/
+		while(!stack.isEmpty()) {
+			sb.append(stack.pop()).append(" ");
+		}
+		System.out.println(sb);
+	}
+	
+	static class Node implements Comparable<Node>{
+		public int to, cost;
+		
+		Node(int to, int cost){
+			this.to = to;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			// TODO Auto-generated method stub
+			return cost - o.cost;
+		}
+		
 	}
 }
